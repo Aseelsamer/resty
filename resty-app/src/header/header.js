288905b723a9306ef2d5f359b5aa9f17e4f8ab01
Aseelsamer <assoul_08@hotmail.com>
2021-02-04 00:54:27 +0200

    cc29

--- resty-app/src/header/header.js
+++ resty-app/src/header/header.js
@@ -1,11 +1,28 @@
 import './header.scss';
+import {Link, NavLink} from 'react-router-dom';
 
-const Header = () => {
-    return (
-      <header>
-          <h1>RESTy</h1>
-      </header>
-    )
-  }
+ class Header extends React.Component{
+     render(){
+        return (
+            <header>
+            <h1>RESTy APP</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/history">History</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/help">About US</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+          )
+     }
+ }
 
-  export default Header;
+
+export default Header;
